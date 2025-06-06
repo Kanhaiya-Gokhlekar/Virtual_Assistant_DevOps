4f72a313a096b1e3fb1bf757731bb52af3785168
Kanhaiya-Gokhlekar <kanha9981671989@gmail.com>
2025-06-06 15:40:35 +0530

    changing port in backend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 10000
 app.use(cors(
     {
         origin:"http://localhost:3000",
